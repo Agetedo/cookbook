c738fdd6f719fd19317edf83bf4e72a91d4001ec
Agetedo <agetedo@gmail.com>
2024-09-21 04:45:07 +0300

    add pages

---
+++ src/pages/RecipePage01.tsx
@@ -0,0 +1,9 @@
+//import Contact from "../components/contact/Contact.tsx";
+
+export default function RecipePage01() {
+  return (
+    <>
+      RecipePage01 Croutons with omelette
+    </>
+  );
+}
---
+++ src/pages/RecipePage02.tsx
@@ -0,0 +1,9 @@
+//import Contact from "../components/contact/Contact.tsx";
+
+export default function RecipePage02() {
+  return (
+    <>
+      RecipePage02 Chicken broth with vegetables
+    </>
+  );
+}
