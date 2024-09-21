dedc7920b85870fdaabf7c2253a99e7e199f0129
Agetedo <agetedo@gmail.com>
2024-09-21 05:18:12 +0300

    add pages

---
+++ src/pages/RecipePage03.tsx
@@ -0,0 +1,9 @@
+import Recipe from "../components/Recipe";
+
+export default function RecipePage03() {
+  return (
+    <>
+      <Recipe title={"Poached eggs on rye buns"} />
+    </>
+  );
+}
---
+++ src/pages/RecipePage04.tsx
@@ -0,0 +1,9 @@
+import Recipe from "../components/Recipe";
+
+export default function RecipePage04() {
+  return (
+    <>
+      <Recipe title={"Baked salmon with herbs"} />
+    </>
+  );
+}
