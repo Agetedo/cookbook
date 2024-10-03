6f438d580337cc3cc7c6e4162f1636933ed01d30
Agetedo <agetedo@gmail.com>
2024-10-03 05:57:44 +0300

    add pages

--- src/pages/RecipePage07.tsx
+++ src/pages/RecipePage07.tsx
@@ -1,11 +1,13 @@
 import Recipe from "../components/Recipe";
 import AdviceGallery from "../components/AdviceGallery";
+import SubscribePortal from "../components/SubscribePortal";
 
 export default function RecipePage07() {
   return (
     <>
       <Recipe title={"Grilled shrimp salad"} />
       <AdviceGallery description={"Grilled shrimp salad"} />
+      <SubscribePortal />
     </>
   );
 }
--- src/pages/RecipePape08.tsx
+++ src/pages/RecipePape08.tsx
@@ -1,11 +1,13 @@
 import Recipe from "../components/Recipe";
 import AdviceGallery from "../components/AdviceGallery";
+import SubscribePortal from "../components/SubscribePortal";
 
 export default function RecipePage08() {
   return (
     <>
       <Recipe title={"Meatloaf with prunes"} />
       <AdviceGallery description={"Meatloaf with prunes"} />
+      <SubscribePortal />
     </>
   );
 }
