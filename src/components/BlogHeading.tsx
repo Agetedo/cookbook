c1d28f89dec71268d90b9baf1fd96945d5817fe7
Agetedo <agetedo@gmail.com>
2024-10-06 18:35:23 +0300

    blogHeading data

--- src/components/BlogHeading.tsx
+++ src/components/BlogHeading.tsx
@@ -1,5 +1,5 @@
 import Conteiner from "./Conteiner";
-import styles from "../styles/BlogHeading.module.scss"
+import styles from "../styles/BlogHeading.module.scss";
 
 export default function BlogHeading() {
   return (
