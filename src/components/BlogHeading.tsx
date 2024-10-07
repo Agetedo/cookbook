ad1bc7090e355177c5b4685d30a6a18d36c57154
Agetedo <agetedo@gmail.com>
2024-10-07 13:13:58 +0300

    add props

--- src/components/BlogHeading.tsx
+++ src/components/BlogHeading.tsx
@@ -1,10 +1,33 @@
 import Conteiner from "./Conteiner";
+import { Link } from "react-router-dom";
+import PathConstants from "../routes/pathConstants";
 import styles from "../styles/BlogHeading.module.scss";
+const publishText = <>{"We don't publish a new CookBook recipe until we're certain it's the tastiest version out there, and we’re constantly updating our old recipes with new techniques and improvements based on further testing and reader suggestions."}</>;
 
 export default function BlogHeading() {
   return (
     <Conteiner className={styles.blogHeading}>
-     
+      <BlogTitle 
+        recipesLink={"Back to Recipes"} 
+        title={"CookBook the secret ingredient for home cooks!"} 
+        text={publishText} />
     </Conteiner>
+  );
+}
+
+interface BlogTitleProps {
+  recipesLink: string;
+  title: string;
+  text: string | typeof publishText;
+}
+function BlogTitle({ recipesLink, title, text }: BlogTitleProps) {
+  return (
+    <>
+      <Link to={PathConstants.SELECTEDPAGE} className={styles.titleLink}>
+        {recipesLink}
+      </Link>
+      <h1 className={styles.blogTitle}>{title}</h1>
+      <p className={styles.blogText}>{text}</p>
+    </>
   );
 }
