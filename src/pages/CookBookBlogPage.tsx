a12b960a91003ebaa8255e88b9d147b30751d6e3
Agetedo <agetedo@gmail.com>
2024-10-08 20:56:33 +0300

    PopularPosts

--- src/pages/CookBookBlogPage.tsx
+++ src/pages/CookBookBlogPage.tsx
@@ -1,11 +1,13 @@
 import BlogHeading from "../components/BlogHeading";
 import FeaturedPosts from "../components/FeaturedPosts";
+import PopularPosts from "../components/PopularPosts";
 
 export default function CookBookBlogPage() {
   return (
     <>
       <BlogHeading />
       <FeaturedPosts />
+      <PopularPosts />
     </>
   );
 }
