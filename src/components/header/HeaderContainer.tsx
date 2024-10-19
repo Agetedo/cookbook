cc9e0207a182b275ea3a927c43802d81e06b686b
Agetedo <agetedo@gmail.com>
2024-10-19 21:56:07 +0300

    interface HeaderContainerProps

--- src/components/header/HeaderContainer.tsx
+++ src/components/header/HeaderContainer.tsx
@@ -5,7 +5,6 @@ interface HeaderContainerProps {
   className: string;
   children: React.ReactNode;
 }
-
 export default function HeaderConteiner({ children, className }: HeaderContainerProps) {
   return (
     <header className={className}>
